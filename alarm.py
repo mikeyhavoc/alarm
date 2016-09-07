aa2a42008ea044f89c82804311fa90545e66d69c
mikeyhavoc <m.jwilliams@outlook.com>
2016-09-07 19:17:37 -0400

    add asserts in code, refactor code, mod hour and mod minute, del old test to compare time, add wake_up_time def

--- alarm.py
+++ alarm.py
@@ -2,53 +2,51 @@ import datetime, time, webbrowser
 
 
 def hour():
+    '''hour - gets the hour from datetime,
+      24 clock if (hour > 12) - 12 hours  =  time
+      else hours = time
+      return  time. '''
     hour = datetime.datetime.now().hour
 
     hours = hour
     time = hours
     if (hour > 12):
+
+
         time =  hours - 12
+        assert hours > 12, 'get time hour subtraction needs fix'
+
     else:
         time = hours
+        assert hours > 12, 'get time hour no sub needs fix'
 
     return  time
 
 def minute():
     minute = datetime.datetime.now().minute
+   #minute from datetime 0 to 60
+    return minute
 
-    minutes = minute
+def wake_up_time():
+    '''alarm wake up time.'''
+    time = input('when would you like to wake up? eg 6:45, 10:30 ')
+    if len(time) == 4: # grabbing single digit hours before :
+        hour = time[0]
+        print(hour)
+    elif len(time) == 5: # grabbing double digit hours before :
+        hour = time[0:2]
 
-    return minutes
 
 
 
-def alarm(h,m):
-    hour = datetime.datetime.now().hour
 
-    hours = hour
-    time = hours
-    if (hour > 12):
-        h = hours - 12
-    else:
-        h = hours
 
-    minute = datetime.datetime.now().minute
 
-    m = minute
 
-    return  h +  m
 
 
 
 h = hour()
+print(str(h))
 m = minute()
-wake = alarm(h,m)
-
-def clock(h,m):
-    if wake  ==  h + m :
-        print('wake up')
-    else:
-        print('sleep')
-
-clock(h,m)
-#print(str(h) +':' + str(m))
+wake = wake_up_time()
