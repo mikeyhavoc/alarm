8362bcc41d19383ec9abb7778a5a9a94694a47a9
mikeyhavoc <m.jwilliams@outlook.com>
2016-09-14 11:28:38 -0400

    update to README

--- README.md
+++ README.md
@@ -3,11 +3,10 @@
 python program
 right now designed just for EST
 
-it presents the time in hours and minutes.
-
-you enter the hour:minute
+Asks for you to enter the hour:minute
 you would like to wake up at 
-it then has a designated youtube url which will open at this time.
+future -> it then has a designated youtube url which will open at this 
+time.
 
 to turn off you must get up and close it.
 
--- alarm.py
+++ alarm.py
@@ -1,6 +1,5 @@
 import datetime, time, webbrowser
 
-
 def hour():
     '''hour - gets the hour from datetime,
       24 clock if (hour > 12) - 12 hours  =  time
@@ -11,7 +10,6 @@ def hour():
     hours = hour
     time = hours
     if (hour > 12):
-
 
         time =  hours - 12
         assert hours > 12, 'get time hour subtraction needs fix'
@@ -27,23 +25,32 @@ def minute():
    #minute from datetime 0 to 60
     return minute
 
-
-
-def wake_up_time(h, m):
+def wake_up_time():
     '''alarm wake up time.'''
+    h = 0
+    m = 0
     time = input('when would you like to wake up? eg 6:45, 10:30 ')
-    if len(time) == 4: # grabbing single digit hours before :
-        hour = int(time[0])
-        assert  int(time[0]) > 0, 'time < 1'
-    elif len(time) == 5: # grabbing double digit hours before :
-        hour = int(time[0:2])
-        assert int(time[0:2]) < 13, ' time < 12'
-    minutes = int(time[3:6])
+    if len(time) == 5: # grabbing single digit hours before :
+        hour = int(time[0:1])
+        hour = h
+        assert  int(time[0:1]) > 0, 'time < 1'
+    if len(time) == 4: #grabbing double digit for minutes if hours single
+        minutes = int(time[2:5])
+        m = minutes
+    elif len(time) == 6 : # grabbing double digit hours before :
+        hour = int(time[1:3])
+        h = hour
+    assert int(time[0:2]) < 13, ' time < 12'
+    else:
+        minutes = int(time[3:6])
+         m = minutes
+    return '%d : %d ' %(h, m)
 
-    #wake_up = time.strftime('%I + : + %M', t % h, m)
+#def time_to_wake_up
 
 
 
-h = hour()
-m = minute()
-wake_up_time(h,m)
+#h = hour()
+#m = minute()
+time = wake_up_time()
+print(time)
